dd02ad6f8b4e26172be3cc9d8a74b4228101238b
kirillGulyaev <kgulyaev52@mail.ru>
2022-02-06 03:57:48 +0700

    Remove Procfile

--- server/bin/server.js
+++ server/bin/server.js
@@ -3,7 +3,8 @@
 import getApp from "../index.js";
 
 const port = process.env.PORT || 5000;
+const address = "0.0.0.0";
 
-getApp().listen(port, () => {
+getApp().listen(port, address, () => {
   console.log(`Server is running on port: ${port}`);
 });
